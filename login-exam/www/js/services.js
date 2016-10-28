62187fc462929707e2d6101f5e55c9a320123e69
Song <iamsjh1017@gmail.com>
2016-10-28 20:36:44 +0900

    divided services from app.js and created AuthService

---
+++ login-exam/www/js/services.js
@@ -0,0 +1,248 @@
+// TODO: 로그아웃, 정보변경, 회원탈퇴 등등
+// 회원 관련 서비스
+app.factory('AuthService', function ($location, $firebaseAuth, $cordovaOauth, $http, MyPopup, Localstorage) {
+  return {
+    // 이메일 로그인 메소드
+    loginWithEmail: function (useremail, password, redirectTo) {
+      if ($firebaseAuth().$getAuth()) {
+        $firebaseAuth().$signOut();
+        Localstorage.remove('user');
+        console.log('로그아웃');
+        $firebaseAuth().$signOut();
+      } else {
+        $firebaseAuth().$signInWithEmailAndPassword(useremail, password)
+          .then(function (result) {
+            // 이메일 인증 확인
+            if (!result.emailVerified) {
+              MyPopup.show('로그인 실패', '이메일 인증이 필요합니다.')
+              $firebaseAuth().$signOut();
+              return;
+            }
+            console.log('이메일 로그인 성공');
+            // 로그인 성공 후 Localstorage에 유저정보 저장
+            Localstorage.setObject("user", result);
+
+            // 로그인 성공후 페이지 이동
+            $location.path(redirectTo);
+          })
+          .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode === 'auth/wrong-password') {
+              console.log('로그인 실패 : 비밀번호가 잘못되었습니다.');
+              MyPopup.show('로그인 실패', '비밀번호가 잘못되었습니다.');
+            } else {
+              console.log('로그인 실패 : ' + errorMessage);
+              MyPopup.show('로그인 실패', errorMessage);
+            }
+          });
+      }
+    },
+
+    // 소셜 로그인 메소드
+    loginWithSocial: function (providerName, redirecTo) {
+
+      // TODO: 소셜 로그인 메소드
+      // TODO: 로그인 성공 후 $localstorage에 인증 정보 저장
+      // TODO: MySQL DB 유저정보에 있는지 확인하고 있으면 UPDATE 없으면 INSERT
+      if ($firebaseAuth().$getAuth()) {
+        $firebaseAuth().$signOut();
+        Localstorage.remove('user');
+        console.log('로그아웃');
+      } else {
+        switch (providerName) {
+          case "google":
+            if (ionic.Platform.isWebView()) {
+              $cordovaOauth.google("506479374537-4o2pa5ghuj68ocudca9fbohmikfsth56.apps.googleusercontent.com" + "&include_profile=true", ["email", "profile"]).then(function (result) {
+                var credential = firebase.auth.GoogleAuthProvider.credential(result.id_token);
+                console.log('----------------credential--------------');
+                console.log(result);
+                console.log(result.id_token);
+                console.log(result.access_token);
+                console.log(credential);
+                console.log('----------------------------------------');
+                firebase.auth().signInWithCredential(credential).then(function (authData) {
+                  MyPopup.show('알림', '구글 로그인성공');
+                  Localstorage.setObject('user', JSON.stringify(authData));
+                  $location.path(redirecTo);
+                }, function (error) {
+                  console.error("firebase: " + error);
+                });
+              }, function (error) {
+                console.error("ERROR: " + error);
+              });
+            } else {
+              var provider = new firebase.auth.GoogleAuthProvider();
+              provider.addScope('email');
+              provider.addScope('profile');
+              firebase.auth().signInWithPopup(provider).then(function (result) {
+                $myPopup.show('알림', '구글 로그인성공');
+              });
+            }
+            break;
+
+          case "facebook":
+            // 페북에서 Oauth토근 가져와서
+            $cordovaOauth.facebook("947628548702706", ["email"]).then(function (result) {
+              // Firebase에 토큰 가져가서 인증
+              $firebaseAuth().$authWithOAuthToken("facebook", result.access_token).then(function (authData) {
+                console.log("소셜로그인성공")
+                console.log(JSON.stringify(authData));
+                $localstorage.setObject('user', authData)
+                $location.path('/loginMain');
+              }, function (error) {
+                console.error("ERROR: " + error);
+              });
+            }, function (error) {
+              console.error("ERROR: " + error);
+            });
+            break;
+
+          case "twitter":
+            $cordovaOauth.twitter("CLIENT_ID_HERE", "CLIENT_SECRET_HERE").then(function (result) {
+              console.log(JSON.stringify(result));
+            }, function (error) {
+              console.error("ERROR: " + error);
+            });
+            break;
+
+        }
+      }
+    },
+
+    // 로그아웃 메소드
+    logout: function () {
+      $firebaseAuth().$signOut();
+      Localstorage.remove('user');
+      console.log('로그아웃');
+    },
+
+    // 이메일 회원가입 메소드
+    register: function (username, useremail, password, redirectTo) {
+      console.log('회원가입');
+      $firebaseAuth().$createUserWithEmailAndPassword(useremail, password)
+        .then(function (result) {
+
+          // 회원정보 파싱
+          var uid = result.uid;
+          var email = result.email;
+          var photoURL = result.photoURL;
+          var providerId = result.providerId;
+          var providerData = result.providerData;
+
+          // 회원정보 DB 입력
+          $http({
+            url: 'http://localhost:3001/rscamper-server/app/user/insert',
+            method: 'POST',
+            data: $.param({
+              userUid: uid,
+              displayName: username,
+              email: email,
+              photoUrl: photoURL,
+              providerName: providerId,
+              providerUid: providerData.uid,
+              providerDisplayName: providerData.displayName,
+              providerPhotoUrl: providerData.photoUrl,
+              providerEmail: providerData.email
+            }),
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            }
+          }).success(function () {
+            console.log('회원정보 DB입력 성공');
+            // 인증 메일 발송
+            firebase.auth().currentUser.sendEmailVerification().then(function () {
+              MyPopup.show('회원가입 성공', '계정 활성화를 위해 이메일 인증을 해주시기 바랍니다.');
+              // 회원가입 완료후 로그인페이지로 이동
+              $location.path(redirectTo);
+            });
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    },
+
+    // 비밀번호 초기화 메소드
+    resetPassword: function (useremail) {
+      firebase.auth().sendPasswordResetEmail(useremail).then(function () {
+        MyPopup.show('알림', '비밀번호 초기화 메일이 발송 되었습니다. 이메일을 확인해 주세요!');
+        $location.path('/signin');
+      }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == 'auth/invalid-email') {
+          MyPopup.show('에러', '잘못된 이메일 주소입니다.');
+        } else if (errorCode == 'auth/user-not-found') {
+          MyPopup.show('에러', '요청한 이메일이 존재하지 않습니다.');
+        }
+        console.log(error);
+      });
+    },
+
+    // 인증메일 재발송
+    sendVerifyEmail: function (email) {
+
+    },
+
+    // 로그인한 유저정보 가져오기
+    getCurrentUser: function () {
+      return Localstorage.getObject('user');
+    },
+
+    // 회원탈퇴 메소드
+    resign: function () {
+      var user = firebase.auth().currentUser;
+      user.delete().then(function() {
+        // User deleted.
+      }, function(error) {
+        // An error happened.
+      });
+    },
+
+    // 회원정보 수정 메소드
+    update: function () {
+
+    }
+  };
+})
+
+
+
+
+
+// [유틸] localStorage사용을 위한 셋팅
+  .factory('Localstorage', ['$window', function ($window) {
+    return {
+      set: function (key, value) {
+        $window.localStorage[key] = value;
+      },
+      get: function (key, defaultValue) {
+        return $window.localStorage[key] || defaultValue;
+      },
+      setObject: function (key, value) {
+        $window.localStorage[key] = JSON.stringify(value);
+      },
+      getObject: function (key) {
+        return JSON.parse($window.localStorage[key] || '{}');
+      },
+      remove: function (key) {
+        $window.localStorage.removeItem(key);
+      }
+    }
+  }])
+
+  // [유틸] 팝업창 사용을 위한 서비스
+  .factory('MyPopup', ['$ionicPopup', function ($ionicPopup) {
+    return {
+      show: function (title, template) {
+        $ionicPopup.alert({
+          title: title,
+          template: template
+        })
+          .then(function (res) {
+          });
+      }
+    }
+  }]);
+
--- login-exam/www/views/login/loginControllers.js
+++ login-exam/www/views/login/loginControllers.js
@@ -1,223 +1,30 @@
 // 로그인 컨트롤러
-app.controller('SigninController', function ($scope, $auth, $location, $localstorage, $cordovaOauth, $myPopup) {
-  // 인증 객체 스코프에 저장
-  $scope.auth = $auth;
-  console.log($auth);
-
+app.controller('SigninController', function ($scope, AuthService) {
   // TODO: 로그인 폼 유효성 검사
 
-  // 이메일 로그인 메소드
-  $scope.loginWithEmail = function (useremail, password) {
-    if ($scope.auth.$getAuth()) {
-      console.log("로그아웃됨");
-      $scope.auth.$signOut();
-    } else {
-      $scope.auth.$signInWithEmailAndPassword(useremail, password)
-        .then(function (result) {
-          // 이메일 인증 확인
-          if (!result.emailVerified) {
-            $myPopup.show('로그인 실패', '이메일 인증이 필요합니다.')
-            $scope.auth.$signOut();
-            return;
-          }
-          console.log('이메일 로그인 성공');
-          // 로그인 성공 후 $localstorage에 유저정보 저장
-          $localstorage.setObject("user", result);
-
-          // 로그인 성공후 페이지 이동
-          $location.path('/loginMain');
-        })
-        .catch(function (error) {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          if (errorCode === 'auth/wrong-password') {
-            console.log('로그인 실패 : 비밀번호가 잘못되었습니다.');
-            $myPopup.show('로그인 실패', '비밀번호가 잘못되었습니다.');
-          } else {
-            console.log('로그인 실패 : ' + errorMessage);
-            $myPopup.show('로그인 실패', errorMessage);
-          }
-        });
-    }
-  };
-
-  // TODO: 소셜 로그인 메소드
-  // TODO: 로그인 성공 후 $localstorage에 인증 정보 저장
-  // TODO: MySQL DB 유저정보에 있는지 확인하고 있으면 UPDATE 없으면 INSERT
-  $scope.loginWithPortal = function (providerName) {
-    if ($scope.auth.$getAuth()) {
-      console.log("로그아웃됨");
-      $scope.auth.$signOut();
-    } else {
-
-      switch (providerName) {
-        case "google":
-          if(ionic.Platform.isWebView()) {
-            $cordovaOauth.google("506479374537-4o2pa5ghuj68ocudca9fbohmikfsth56.apps.googleusercontent.com" + "&include_profile=true", ["email", "profile"]).then(function (result) {
-              var credential = firebase.auth.GoogleAuthProvider.credential(result.id_token);
-              console.log('----------------credential--------------');
-              console.log(result);
-              console.log(result.id_token);
-              console.log(result.access_token);
-              console.log(credential);
-              console.log('----------------------------------------');
-              firebase.auth().signInWithCredential(credential).then(function (authData) {
-                $myPopup.show('알림', '구글 로그인성공');
-                $localstorage.setObject('user', JSON.stringify(authData));
-                $location.path('/loginMain');
-              }, function (error) {
-                console.error("firebase: " + error);
-              });
-            }, function (error) {
-              console.error("ERROR: " + error);
-            });
-          } else {
-            var provider = new firebase.auth.GoogleAuthProvider();
-            provider.addScope('email');
-            provider.addScope('profile');
-            firebase.auth().signInWithPopup(provider).then(function (result) {
-              $myPopup.show('알림', '구글 로그인성공');
-            });
-          }
-          break;
-
-        case "facebook":
-          // 페북에서 Oauth토근 가져와서
-          $cordovaOauth.facebook("947628548702706", ["email"]).then(function (result) {
-            // Firebase에 토큰 가져가서 인증
-            $scope.auth.$authWithOAuthToken("facebook", result.access_token).then(function (authData) {
-              console.log("소셜로그인성공")
-              console.log(JSON.stringify(authData));
-              $localstorage.setObject('user', authData)
-              $location.path('/loginMain');
-            }, function (error) {
-              console.error("ERROR: " + error);
-            });
-          }, function (error) {
-            console.error("ERROR: " + error);
-          });
-          break;
-
-        case "twitter":
-          $cordovaOauth.twitter("CLIENT_ID_HERE", "CLIENT_SECRET_HERE").then(function (result) {
-            console.log(JSON.stringify(result));
-          }, function (error) {
-            console.error("ERROR: " + error);
-          });
-          break;
-
-      }
-    }
-  }
+  $scope.loginWithEmail = AuthService.loginWithEmail;
+  $scope.loginWithPortal = AuthService.loginWithSocial;
 })
 
 // 회원가입 컨트롤러
-  .controller('SignupController', ['$scope', '$auth', '$location', '$localstorage', '$myPopup', '$http',
-    function ($scope, $auth, $location, $localstorage, $myPopup, $http) {
-      $scope.auth = $auth;
+  .controller('SignupController', function ($scope, AuthService) {
+    // TODO: 회원가입 폼 유효성 검사
 
-
-      // TODO: 회원가입 폼 유효성 검사
-
-      // 이메일 회원 등록 메소드
-      $scope.register = function (username, useremail, password) {
-        console.log('회원가입');
-        $scope.auth.$createUserWithEmailAndPassword(useremail, password)
-          .then(function (result) {
-            // TODO: 회원가입 성공시 MySQL DB에 Insert
-            var uid = result.uid;
-            result.displayName = username;
-            var displayName = result.displayName;
-            var email = result.email;
-            var photoURL = result.photoURL;
-            var providerId = result.providerId;
-            var providerData = result.providerData;
-            console.log('uid : ' + uid);
-            console.log('displayName : ' + displayName);
-            console.log('email : ' + email);
-            console.log('photoURL : ' + photoURL);
-            console.log('providerId :  ' + providerId);
-            console.log('providerData : ' + providerData);
-
-            $http({
-              url: 'http://localhost:3001/rscamper-server/app/user/insert',
-              method: 'POST',
-              data: $.param({
-                userUid: uid,
-                displayName: displayName,
-                email: email,
-                photoUrl: photoURL,
-                providerName: providerId,
-                providerUid: providerData.uid,
-                providerDisplayName: providerData.displayName,
-                providerPhotoUrl: providerData.photoUrl,
-                providerEmail: providerData.email
-              }),
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-              }
-            }).success(function () {
-              console.log('회원정보 MYSQL DB입력 성공');
-              // 인증 메일 발송
-              firebase.auth().currentUser.sendEmailVerification().then(function () {
-                $myPopup.show('회원가입 성공', '계정 활성화를 위해 이메일 인증을 해주시기 바랍니다.');
-                // 회원가입 완료후 로그인페이지로 이동
-                $location.path('/signin');
-              });
-            });
-
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
-
-
-      };
-    }])
+    $scope.register = AuthService.register;
+  })
 
   // 비밀번호 초기화 컨트롤러
-  .controller('ResetPasswordController', ['$scope', '$auth', '$location', '$localstorage', '$myPopup',
-    function ($scope, $auth, $location, $localstorage, $myPopup) {
-      $scope.auth = $auth;
-      $scope.resetPassword = function (useremail) {
-        firebase.auth().sendPasswordResetEmail(useremail).then(function () {
-          $myPopup.show('알림', '비밀번호 초기화 메일이 발송 되었습니다. 이메일을 확인해 주세요!');
-          $location.path('/signin');
-        }).catch(function (error) {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          if (errorCode == 'auth/invalid-email') {
-            $myPopup.show('에러', '잘못된 이메일 주소입니다.');
-          } else if (errorCode == 'auth/user-not-found') {
-            $myPopup.show('에러', '요청한 이메일이 존재하지 않습니다.');
-          }
-          console.log(error);
-        });
-      };
-    }])
+  .controller('ResetPasswordController', function ($scope, AuthService) {
+    $scope.resetPassword = AuthService.resetPassword;
+  })
 
   // 로그인 메인 컨트롤러
-  .controller('LoginMainController', ['$scope', '$auth', '$location', '$localstorage',
-    function ($scope, $auth, $location, $localstorage) {
-      $scope.auth = $auth;
-      var user = $localstorage.get('user');
-      console.log('유저정보로딩', user);
+  .controller('LoginMainController', function ($scope, AuthService) {
 
-      // TODO: 로그인 정보가 있으면 로그아웃 버튼 활성화, 없으면 로그인 버튼 활성화
-      if (user) {
+    var user = AuthService.getCurrentUser();
+    $scope.user = user;
+    console.log('유저정보로딩', user);
 
-      } else {
+    $scope.logout = AuthService.logout;
 
-      };
-
-      $scope.user = user;
-      $scope.img = '/img/ionic.png';
-
-    }])
-
-  // TODO: 로그아웃, 정보변경, 회원탈퇴 등등
-  .controller('AccountController', ['$scope', '$auth', '$location', '$localstorage',
-    function ($scope, $auth, $location, $localstorage) {
-      $scope.auth = $auth;
-
-    }]);
+  });
